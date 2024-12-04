de979dc0872651de682b3f6d5409aa20c4c911fe
Guilherme Silva <guilherme.cedesp01@gmail.com>
2024-12-04 19:37:09 -0300

    Add files via upload

---
+++ relogio.js
@@ -0,0 +1,26 @@
+const caixaHoras = document.getElementById('horas');
+const caixaMinutos = document.getElementById('minutos');
+const caixaSegundos = document.getElementById('segundos');
+const caixaDate = document.getElementById('date');
+
+const relogio = setInterval(() => {
+    
+    let dateToday = new Date();
+
+    const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
+
+    let date = dateToday.toLocaleDateString('pt-BR', options);
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let sec = dateToday.getSeconds();
+
+    if(hr < 10 )  hr = "0" + hr;
+    if(min < 10 )  min = "0" + min;
+    if(sec < 10 )  sec = "0" + sec;
+
+    caixaDate.textContent = date;
+    caixaHoras.textContent = hr;
+    caixaMinutos.textContent = min;
+    caixaSegundos.textContent = sec;
+
+}, 10);
